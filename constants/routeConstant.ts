00d887df9babfde0f72a449dccea5dcd86036ffe
ShakilJoy31 <joy31a@gmail.com>
2024-09-26 01:33:21 +0600

    Url should be changed

--- constants/routeConstant.ts
+++ constants/routeConstant.ts
@@ -1,7 +1,7 @@
 // Production URL
-// export const BASE_URL = 'https://legal-estate-server.onrender.com/api/v1/user';
+export const BASE_URL = 'https://legal-estate-server.onrender.com/api/v1/';
 // Development URL
-export const BASE_URL:string = 'http://localhost:5000/api/v1/';
+// export const BASE_URL:string = 'http://localhost:5000/api/v1/';
 
 
 export const userRegistration:string = 'user/create-user'
